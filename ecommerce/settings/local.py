01ed5359bdeadb8ed0da12abe508a249a304799b
Yash Goyal <yashgo0018@gmail.com>
2019-10-24 21:40:04 +0530

    initial commit

---
+++ ecommerce/settings/local.py
@@ -0,0 +1,16 @@
+from .base import *
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# Add Your Razorpay keys
+RAZORPAY_KEY_ID = 'rzp_test_Xs4cMHfADoX6DU'
+RAZORPAY_KEY_SECRET = 'MzgRhzXb5bVJYqUPVHAdEOhR'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
