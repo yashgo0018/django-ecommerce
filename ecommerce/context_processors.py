8fd23abed819f82a3c7438ec986728b68da143f2
Yash Goyal <yashgo0018@gmail.com>
2019-10-24 23:34:29 +0530

    added dynamic currency symbol

--- ecommerce/context_processors.py
+++ ecommerce/context_processors.py
@@ -2,4 +2,4 @@ from django.conf import settings
 
 
 def currency_info(request):
-    return {'currency_symbol': settings.CURRENCY.symbol, 'currency_code': settings.CURRENCY.code}
+    return {'currency_symbol': settings.CURRENCY.get('symbol'), 'currency_code': settings.CURRENCY.get('code')}
--- products/forms.py
+++ products/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.urls import resolve
 from django.conf import settings
 
-symbol = settings.CURRENCY.symbol
+symbol = settings.CURRENCY.get('symbol')
 
 
 class FilterForm(forms.Form):
