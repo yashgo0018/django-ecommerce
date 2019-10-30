03f924abe711ea0b278e2481e850f6fa31080df0
Yash Goyal <yashgo0018@gmail.com>
2019-10-30 14:55:36 +0530

    initial commit

--- ecommerce/settings/local.py
+++ ecommerce/settings/local.py
@@ -1,5 +1,18 @@
 from .base import *
 import dj_database_url
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
 
 # Add Your Razorpay keys
 RAZORPAY_KEY_ID = 'rzp_test_Xs4cMHfADoX6DU'
