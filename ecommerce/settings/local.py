19dcce4acf7e70e9a28969ecda48c15d3e9399ee
Yash Goyal <yashgo0018@gmail.com>
2019-10-30 14:46:57 +0530

    initial commit

--- ecommerce/settings/local.py
+++ ecommerce/settings/local.py
@@ -4,3 +4,7 @@ from .base import *
 # Add Your Razorpay keys
 RAZORPAY_KEY_ID = 'rzp_test_Xs4cMHfADoX6DU'
 RAZORPAY_KEY_SECRET = 'MzgRhzXb5bVJYqUPVHAdEOhR'
+DATABASES['default'] = {
+    'ENGINE': 'django.db.backends.postgresql_psycopg2'
+}
+DATABASES['default'].update(dj_database_url.config(conn_max_age=500))
