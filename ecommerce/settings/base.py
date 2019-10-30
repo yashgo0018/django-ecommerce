4fed3633b208842dd65026f2084865a1d2c445a9
Yash Goyal <yashgo0018@gmail.com>
2019-10-30 15:42:48 +0530

    initial commit

--- ecommerce/settings/base.py
+++ ecommerce/settings/base.py
@@ -55,6 +55,7 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
             os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'staticfiles')
         ],
         'APP_DIRS': True,
         'OPTIONS': {
