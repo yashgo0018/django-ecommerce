5356ca8178b78918acdd012b96d39a1243d7c2ed
Yash Goyal <yashgo0018@gmail.com>
2019-10-30 14:42:50 +0530

    initial commit

--- ecommerce/settings/base.py
+++ ecommerce/settings/base.py
@@ -9,7 +9,7 @@ SECRET_KEY = 'ks2zg!5l!0w#qwwp_=l*q*vio-!yxioq6n#qo_pzo5to77f2n-'
 DEBUG = True  # if os.environ.get('PRODUCTION') == '1' else True
 
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['localhost', 'python-ecom-app.herokuapp.com']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
