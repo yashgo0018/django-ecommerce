e5f311f3adca34b0d474e90a59f7a57c0ac06595
Yash Goyal <yashgo0018@gmail.com>
2019-10-30 15:04:28 +0530

    initial commit

--- ecommerce/settings/prod.py
+++ ecommerce/settings/prod.py
@@ -1,17 +1,8 @@
-
-import dj_database_url
-from .base import *
-
-ALLOWED_HOSTS = ['python-ecom-app.herokuapp.com']
-DATABASES['default'] = {
-    'ENGINE': 'django.db.backends.postgresql_psycopg2'
-}
-DATABASES['default'].update(dj_database_url.config(conn_max_age=500))
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -19,7 +10,3 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
-# Add Your Razorpay keys
-RAZORPAY_KEY_ID = 'rzp_test_Xs4cMHfADoX6DU'
-RAZORPAY_KEY_SECRET = 'MzgRhzXb5bVJYqUPVHAdEOhR'
