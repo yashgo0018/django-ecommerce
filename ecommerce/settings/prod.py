90d13ca645fe504ecd57dda6a987ea006ee94b75
Yash Goyal <yashgo0018@gmail.com>
2020-03-22 15:37:25 +0530

    Updated WhiteNoise Settings

--- ecommerce/settings/prod.py
+++ ecommerce/settings/prod.py
@@ -5,8 +5,8 @@ from .base import *
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
