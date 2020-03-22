97cfbdc6f3549f0b90d1b070ae68873dc306c45c
Yash Goyal <yashgo0018@gmail.com>
2020-03-22 17:17:34 +0530

    Sorted Out whitenoise manifiest problem

--- ecommerce/settings/prod.py
+++ ecommerce/settings/prod.py
@@ -1,10 +1,8 @@
 import dj_database_url
-import django_heroku
 from .base import *
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATICFILES_STORAGE = 'ecommerce.storage.WhiteNoiseStaticFilesStorage'
 
 MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
 
 DATABASES['default'].update(dj_database_url.config(conn_max_age=500))
-# django_heroku.settings(locals())
