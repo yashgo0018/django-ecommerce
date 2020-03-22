e07136dec56ed735179ecb135d2a790ce98d79e4
Yash Goyal <yashgo0018@gmail.com>
2020-03-22 18:10:05 +0530

    Added Environment variable to github actions

--- ecommerce/settings/base.py
+++ ecommerce/settings/base.py
@@ -17,6 +17,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic',
+
     'django.contrib.staticfiles',
 
     # User Made App
