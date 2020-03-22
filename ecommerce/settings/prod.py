a902edb84796651e0b25c065ca14e30d8f6edda2
Yash Goyal <yashgo0018@gmail.com>
2020-03-22 18:30:51 +0530

    Testing

--- ecommerce/settings/prod.py
+++ ecommerce/settings/prod.py
@@ -1,5 +1,4 @@
 import dj_database_url
-import django_heroku
 from .base import *
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
@@ -7,4 +6,3 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
 
 DATABASES['default'].update(dj_database_url.config(conn_max_age=500))
-django_heroku.settings(locals())
