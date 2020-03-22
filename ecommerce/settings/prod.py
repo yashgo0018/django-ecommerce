27193bd04bd5a7008de6a92441548e25c8a6ad31
Yash Goyal <yashgo0018@gmail.com>
2020-03-22 16:28:48 +0530

    Just A test

--- ecommerce/settings/prod.py
+++ ecommerce/settings/prod.py
@@ -4,17 +4,7 @@ from .base import *
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
-
+MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
 
 DATABASES['default'].update(dj_database_url.config(conn_max_age=500))
 django_heroku.settings(locals())
