f2d9b5499b8f46aeae2e245e58b5fc3b4d98ae48
Yash Goyal <yashgo0018@gmail.com>
2020-03-22 17:11:01 +0530

    Just A test

--- ecommerce/settings/prod.py
+++ ecommerce/settings/prod.py
@@ -2,7 +2,7 @@ import dj_database_url
 import django_heroku
 from .base import *
 
-STATICFILES_STORAGE = 'ecommerce.storage.WhiteNoiseStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
 
