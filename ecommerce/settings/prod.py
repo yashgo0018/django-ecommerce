f63e8c620ed0065e48e2f5d11a120dc9a0b15fbc
Yash Goyal <yashgo0018@gmail.com>
2020-03-26 14:37:03 +0530

    Encapsulated Productions Settings

--- ecommerce/settings/prod.py
+++ ecommerce/settings/prod.py
@@ -5,4 +5,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
 
+INSTALLED_APPS.insert(INSTALLED_APPS.index(
+    'django.contrib.staticfiles') - 1, 'whitenoise.runserver_nostatic')
+
 DATABASES['default'].update(dj_database_url.config(conn_max_age=500))
